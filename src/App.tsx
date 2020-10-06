c631cb7f396de407c6d6cb6fb8be42e51ef1c85f
İlker Çakır <ilker.cakir@betbull.com>
2020-10-07 02:45:37 +0300

    material-ui added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { CssBaseline, Container, createMuiTheme } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import { Footer, Header } from './components';
+const theme = createMuiTheme({
+  overrides: {
+    MuiAppBar: {
+      root: { height: '80px', minHeight: '80px', maxHeight: '80px' },
+    },
+    MuiToolbar: {
+      root: {
+        height: 'inherit !important',
+        maxHeight: 'inherit !important',
+        minHeight: 'inherit !important',
+      },
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App" style={{ fontSize: '24px' }}>
-      Hello
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header />
+        <Container>Hello</Container>
+        <Footer />
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { makeStyles, createStyles, Theme, Typography } from '@material-ui/core';
+
+const Copyright = () => {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      © AUTO1 Group 2020
+    </Typography>
+  );
+};
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    footer: {
+      borderTop: `1px solid ${theme.palette.divider}`,
+      marginTop: theme.spacing(8),
+      padding: theme.spacing(2),
+    },
+  }),
+);
+
+const Footer = () => {
+  const classes = useStyles();
+  return (
+    <footer className={classes.footer}>
+      <Copyright />
+    </footer>
+  );
+};
+
+export default Footer;
