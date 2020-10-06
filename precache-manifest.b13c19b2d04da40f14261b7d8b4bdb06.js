129ad24973868dce82f68f00ba02a242fe7c660c
İlker Çakır <ilker.cakir@betbull.com>
2020-10-07 00:43:00 +0300

    Updates

---
+++ precache-manifest.b13c19b2d04da40f14261b7d8b4bdb06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d251fda69c53da7d977d5b4b6b9f20",
+    "url": "/automobile-app/index.html"
+  },
+  {
+    "revision": "f27188bbbd7ea184bbe0",
+    "url": "/automobile-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "233dd9f1b04d43279eab",
+    "url": "/automobile-app/static/js/2.19094cce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/automobile-app/static/js/2.19094cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f27188bbbd7ea184bbe0",
+    "url": "/automobile-app/static/js/main.d08e8f8a.chunk.js"
+  },
+  {
+    "revision": "900d3b9a06fe833e8d0b",
+    "url": "/automobile-app/static/js/runtime-main.0fdf6f63.js"
+  }
+]);
