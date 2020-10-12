e3555c4f75990a7f887a375079f5ff158439e2e0
İlker Çakır <ilker.cakir@betbull.com>
2020-10-12 11:11:57 +0300

    barrel updated

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
+export { default as CarListItem } from './CarListItem';
+export { default as CarList } from './CarList';
 export { default as Header } from './Header';
 export { default as Footer } from './Footer';
-export { default as CarList } from './CarList';
 export { default as Filters } from './Filters';
