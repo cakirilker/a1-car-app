b917b83b6c774c80e2f2e8e128ede5e64c72dbb8
İlker Çakır <ilker.cakir@betbull.com>
2020-10-12 23:42:44 +0300

    Updates

--- precache-manifest.150a74406651af75617feb5712053262.js
+++ precache-manifest.150a74406651af75617feb5712053262.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e0564a5f2cbc3bad1dafb9796446f500",
+    "revision": "960b58e9382106d91201147aea462049",
     "url": "/a1-car-app/index.html"
   },
   {
-    "revision": "f111a72c214431108aee",
+    "revision": "d6d33bb91677da4ef79b",
     "url": "/a1-car-app/static/css/main.79898294.chunk.css"
   },
   {
-    "revision": "13aae83690fff6353545",
-    "url": "/a1-car-app/static/js/2.ae19760d.chunk.js"
+    "revision": "d153ef069b5e253221bd",
+    "url": "/a1-car-app/static/js/2.2a4dc125.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/a1-car-app/static/js/2.ae19760d.chunk.js.LICENSE.txt"
+    "url": "/a1-car-app/static/js/2.2a4dc125.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f111a72c214431108aee",
-    "url": "/a1-car-app/static/js/main.9d83f10c.chunk.js"
+    "revision": "d6d33bb91677da4ef79b",
+    "url": "/a1-car-app/static/js/main.26603755.chunk.js"
   },
   {
     "revision": "7ce7fa8b62d6510be2ff",
